17ea8c1776006f6d123752aa31e5c5b051099bb4
hspoon-aws <hspoon@amazon.com>
2023-03-08 12:31:59 +0800

    get ffmpeg

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ yum  groupinstall -y "Development Tools"
 yum install -y pcre-devel openssl openssl-devel
 yum install -y ca-certificates openssl openssl-devel stunnel gettext  
 
+
+
+
 # Nginx
 echo "setting up Nginx..."
 ## configure nginx with http-flv-module
@@ -20,13 +23,18 @@ make
 make install
 ls /usr/local/nginx/sbin/nginx
 ln -s /usr/local/nginx/sbin/nginx /usr/local/sbin/nginx
+
+# get ffmpeg
+curl https://runmedia.s3.ap-southeast-1.amazonaws.com/temp/ffmpeg --output /usr/local/nginx/sbin/ffmpeg
+chmod +x /usr/local/sbin/ffmpeg
 ln -s /usr/local/nginx/sbin/ffmpeg /usr/local/sbin/ffmpeg
 cd ..
 
 ## enable nginx service
 echo "setting up Nginx service..."
-cp -f nginx/nginx.service /lib/systemd/system/nginx.service 
-cp -f nginx/nginx.conf /usr/local/nginx/conf/
+yes | cp -rf nginx/nginx.service /lib/systemd/system/nginx.service 
+yes | cp -rf nginx/nginx.conf /usr/local/nginx/conf/
+mkdir -p /usr/local/nginx/
 cp -rf nginx/www/ /usr/local/nginx/
 
 systemctl start nginx.service
@@ -36,14 +44,14 @@ service nginx status
 # sTunnel
 echo "setting up sTunnel ..."
 mkdir -p /var/log/stunnel
-cp -f stunnel/stunnel.conf /etc/stunnel/stunnel.conf
+yes | cp -rf stunnel/stunnel.conf /etc/stunnel/stunnel.conf
 mkdir -p /etc/stunnel/conf.d/
-cp -f stunnel/ivs.conf /etc/stunnel/conf.d/ivs.conf
+yes | cp -rf stunnel/ivs.conf /etc/stunnel/conf.d/ivs.conf
 
 # stunnel /etc/stunnel/stunnel.conf
 
 echo "setting up sTunnel service..."
-cp -f stunnel/stunnel.service /lib/systemd/system/stunnel.service
+yes | cp -f stunnel/stunnel.service /lib/systemd/system/stunnel.service
 systemctl daemon-reload
 systemctl start stunnel.service
 systemctl -l enable stunnel
